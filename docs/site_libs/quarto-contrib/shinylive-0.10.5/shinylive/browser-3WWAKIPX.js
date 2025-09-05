6f0cb9f0f50f6a2d201725ae768a92f7aed541d1
W. Joel Schneider <w.joel.schneider@gmail.com>
2025-09-05 10:12:23 -0400

    apashiny

---
+++ docs/site_libs/quarto-contrib/shinylive-0.10.5/shinylive/browser-3WWAKIPX.js
@@ -0,0 +1,3 @@
+// Shinylive 0.10.5
+// Copyright 2025 Posit, PBC
+import{c as t}from"./chunk-7S5BTB3A.js";var o=t((r,e)=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default o();
